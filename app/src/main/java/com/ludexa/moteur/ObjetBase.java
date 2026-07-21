2d19eba75004933b9034a0fcb4deabaf3918470e
Zinzin <candy.thoury@gmail.com>
2026-07-21 07:57:10 +0200

    Add ObjetBase class with properties and constructor

--- app/src/main/java/com/ludexa/moteur/ObjetBase.java
+++ app/src/main/java/com/ludexa/moteur/ObjetBase.java
@@ -1,1 +1,21 @@
+package com.ludexa.moteur;
 
+import java.util.UUID;
+
+public class ObjetBase {
+    public String id;
+    public String nom;
+    public float x;
+    public float y;
+    public float largeur;
+    public float hauteur;
+
+    public ObjetBase(String nom, float x, float y, float largeur, float hauteur) {
+        this.id = UUID.randomUUID().toString();
+        this.nom = nom;
+        this.x = x;
+        this.y = y;
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+    }
+}
