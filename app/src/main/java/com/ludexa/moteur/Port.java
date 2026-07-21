7a19c0636e3950d795eab7c9fca86a8f06e566bb
Zinzin <candy.thoury@gmail.com>
2026-07-21 07:54:47 +0200

    Add Port class with execution and data types

--- app/src/main/java/com/ludexa/moteur/Port.java
+++ app/src/main/java/com/ludexa/moteur/Port.java
@@ -1,1 +1,22 @@
+public class Port {
 
+    public static final String TYPE_EXECUTION_ENTREE = "EXECUTION_ENTREE";
+    public static final String TYPE_EXECUTION_SORTIE = "EXECUTION_SORTIE";
+    public static final String TYPE_DONNEE_ENTREE   = "DONNEE_ENTREE";
+    public static final String TYPE_DONNEE_SORTIE   = "DONNEE_SORTIE";
+
+    public String nom;
+    public String type;
+    public String valeurSaisie;
+
+    public NoeudBase noeudDestination;
+    public Port portDestination;
+
+    public Port(String nom, String type) {
+        this.nom = nom;
+        this.type = type;
+        this.valeurSaisie = "";
+        this.noeudDestination = null;
+        this.portDestination = null;
+    }
+}
