73dbb1f9e3a3fd6a8b12d7773a5997ea7df4ba34
Zinzin <candy.thoury@gmail.com>
2026-07-21 08:03:30 +0200

    Update Scene.java

--- app/src/main/java/com/ludexa/moteur/Scene.java
+++ app/src/main/java/com/ludexa/moteur/Scene.java
@@ -1,1 +1,24 @@
+package com.ludexa.moteur;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scene {
+    public String nom;
+    public List<ObjetBase> objets;
+    public List<NoeudBase> noeudsLogique;
+
+    public Scene(String nom) {
+        this.nom = nom;
+        this.objets = new ArrayList<>();
+        this.noeudsLogique = new ArrayList<>();
+    }
+
+    public void ajouterObjet(ObjetBase objet) {
+        this.objets.add(objet);
+    }
+
+    public void ajouterNoeud(NoeudBase noeud) {
+        this.noeudsLogique.add(noeud);
+    }
+}
