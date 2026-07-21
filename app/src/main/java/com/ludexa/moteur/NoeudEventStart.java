0d89a92f82aadb3b936be44d9ab6ebf54d7a3046
Zinzin <candy.thoury@gmail.com>
2026-07-21 08:04:11 +0200

    Add NoeudEventStart class for event handling

--- app/src/main/java/com/ludexa/moteur/NoeudEventStart.java
+++ app/src/main/java/com/ludexa/moteur/NoeudEventStart.java
@@ -1,1 +1,15 @@
 
+package com.ludexa.moteur;
+
+public class NoeudEventStart extends NoeudBase {
+
+    public NoeudEventStart() {
+        super(genererId(), "Au Démarrage", "Événement");
+        this.ajouterPort(new Port("Suivant", Port.TYPE_EXECUTION_SORTIE));
+    }
+
+    @Override
+    public void executer() {
+        propagerExecution("Suivant");
+    }
+}
