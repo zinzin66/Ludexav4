d9992700bf474a0b4540aec8ea50d5719878779b
Zinzin <candy.thoury@gmail.com>
2026-07-23 10:06:05 +0200

    Update CanvasBlueprint.java

--- app/src/main/java/com/ludexa/moteur/CanvasBlueprint.java
+++ app/src/main/java/com/ludexa/moteur/CanvasBlueprint.java
@@ -1,11 +1,14 @@
 // haut 1
 package com.ludexa.moteur;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -57,134 +60,50 @@ public class CanvasBlueprint extends View {
         paintPort.setAntiAlias(true);
         
         setBackgroundColor(Color.parseColor("#1E1E1E")); 
-    }
 
-    public void setBlueprint(Blueprint blueprint) {
-        this.blueprintActuel = blueprint;
-        invalidate();
-    }
-
-    public void zoomPlus() {
-        niveauZoom *= 1.25f;
-        invalidate();
-    }
-
-    public void zoomMoins() {
-        niveauZoom /= 1.25f;
-        invalidate();
-    }
-
-    public void zoomReset() {
-        niveauZoom = 1.0f;
-        invalidate();
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.save();
-        canvas.scale(niveauZoom, niveauZoom, getWidth() / 2f, getHeight() / 2f);
-
-        int gridSize = 150; 
-        int w = getWidth();
-        int h = getHeight();
-        int limiteMax = (int) (Math.max(w, h) * 2 / niveauZoom);
-
-        // Grille avec offset
-        for (int i = -limiteMax + (int)(cameraX % gridSize); i < limiteMax; i += gridSize) {
-            canvas.drawLine(i, -limiteMax, i, limiteMax, paintGrille);
-        }
-        for (int i = -limiteMax + (int)(cameraY % gridSize); i < limiteMax; i += gridSize) {
-            canvas.drawLine(-limiteMax, i, limiteMax, i, paintGrille);
-        }
-        
-        canvas.translate(cameraX, cameraY);
-        
-        // Dessin des nœuds du blueprint
-        if (blueprintActuel != null) {
-            for (NoeudBase noeud : blueprintActuel.noeuds) {
-                dessinerNoeud(canvas, noeud);
+        // Gestion du Drag & Drop pour recevoir les nœuds depuis PanneauNoeuds
+        this.setOnDragListener((v, event) -> {
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    return event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN);
+                case DragEvent.ACTION_DRAG_ENTERED:
+                case DragEvent.ACTION_DRAG_LOCATION:
+                case DragEvent.ACTION_DRAG_EXITED:
+                    return true;
+                case DragEvent.ACTION_DROP:
+                    ClipData.Item item = event.getClipData().getItemAt(0);
+                    String typeNoeud = item.getText().toString();
+                    
+                    if (blueprintActuel != null) {
+                        float screenX = event.getX();
+                        float screenY = event.getY();
+                        
+                        // Conversion écran -> scène
+                        float x = ((screenX - getWidth() / 2f) / niveauZoom) + getWidth() / 2f - cameraX;
+                        float y = ((screenY - getHeight() / 2f) / niveauZoom) + getHeight() / 2f - cameraY;
+                        
+                        NoeudBase nouveauNoeud = null;
+                        if ("NoeudEventStart".equals(typeNoeud)) {
+                            nouveauNoeud = new NoeudEventStart();
+                        } else if ("NoeudActionDeplacer".equals(typeNoeud)) {
+                            try {
+                                nouveauNoeud = (NoeudBase) Class.forName("com.ludexa.moteur.NoeudActionDeplacer").newInstance();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        
+                        if (nouveauNoeud != null) {
+                            blueprintActuel.ajouterNoeud(nouveauNoeud, x, y);
+                            invalidate();
+                        }
+                    }
+                    return true;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    return true;
+                default:
+                    return false;
             }
-        }
-        
-        canvas.restore();
+        });
     }
-
-    private void dessinerNoeud(Canvas canvas, NoeudBase noeud) {
-        Float xObj = blueprintActuel.noeudsX.get(noeud.id);
-        Float yObj = blueprintActuel.noeudsY.get(noeud.id);
-        
-        if (xObj == null || yObj == null) return;
-        
-        float x = xObj;
-        float y = yObj;
-        
-        float largeur = 260;
-        int maxPorts = Math.max(noeud.portsEntree.size(), noeud.portsSortie.size());
-        float hauteur = 60 + (maxPorts * 40) + 20; 
-        
-        // Fond principal
-        RectF rectFond = new RectF(x, y, x + largeur, y + hauteur);
-        canvas.drawRoundRect(rectFond, 16, 16, paintNoeudBG);
-        
-        // En-tête
-        RectF rectTitre = new RectF(x, y, x + largeur, y + 45);
-        canvas.drawRoundRect(rectTitre, 16, 16, paintTitreBG);
-        canvas.drawRect(x, y + 25, x + largeur, y + 45, paintTitreBG);
-        
-        // Titre
-        canvas.drawText(noeud.nom, x + 15, y + 32, paintTexteTitre);
-        
-        // Ports d'entrée
-        float startY = y + 70;
-        for (int i = 0; i < noeud.portsEntree.size(); i++) {
-            Port p = noeud.portsEntree.get(i);
-            definirCouleurPort(p);
-            float portY = startY + (i * 40);
-            canvas.drawCircle(x, portY, 8, paintPort);
-            canvas.drawText(p.nom, x + 20, portY + 6, paintTextePort);
-        }
-        
-        // Ports de sortie
-        for (int i = 0; i < noeud.portsSortie.size(); i++) {
-            Port p = noeud.portsSortie.get(i);
-            definirCouleurPort(p);
-            float portY = startY + (i * 40);
-            canvas.drawCircle(x + largeur, portY, 8, paintPort);
-            float textWidth = paintTextePort.measureText(p.nom);
-            canvas.drawText(p.nom, x + largeur - 20 - textWidth, portY + 6, paintTextePort);
-        }
-    }
-    
-    private void definirCouleurPort(Port p) {
-        if (Port.TYPE_EXECUTION_ENTREE.equals(p.type) || Port.TYPE_EXECUTION_SORTIE.equals(p.type)) {
-            paintPort.setColor(Color.WHITE);
-        } else {
-            paintPort.setColor(Color.parseColor("#44AAFF")); 
-        }
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        float x = event.getX();
-        float y = event.getY();
-
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                lastTouchX = x;
-                lastTouchY = y;
-                return true;
-
-            case MotionEvent.ACTION_MOVE:
-                cameraX += (x - lastTouchX) / niveauZoom;
-                cameraY += (y - lastTouchY) / niveauZoom;
-                lastTouchX = x;
-                lastTouchY = y;
-                invalidate(); 
-                return true;
-        }
-        return super.onTouchEvent(event);
-    }
-}
 // bas 1
-            
