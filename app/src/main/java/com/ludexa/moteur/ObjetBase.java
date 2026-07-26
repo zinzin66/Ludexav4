f351b09f4c3747c7486029676c53b163adb93128
Zinzin <candy.thoury@gmail.com>
2026-07-26 17:28:08 +0200

    Update ObjetBase.java

--- app/src/main/java/com/ludexa/moteur/ObjetBase.java
+++ app/src/main/java/com/ludexa/moteur/ObjetBase.java
@@ -1,3 +1,4 @@
+// haut 1
 package com.ludexa.moteur;
 
 import java.util.UUID;
@@ -11,19 +12,18 @@ public class ObjetBase {
     public float y;
     public float largeur;
     public float hauteur;
-    public float rotation; // NOUVEAU : Angle en degrés
+    public float rotation;
 
     public ObjetBase(String nom, float x, float y, float largeur, float hauteur) {
         this.id = UUID.randomUUID().toString();
         this.nom = nom;
-        this.contenuTexte = ""; // Initialisé vide par défaut
+        this.contenuTexte = ""; 
         this.x = x;
         this.y = y;
         this.largeur = largeur;
         this.hauteur = hauteur;
-        this.rotation = 0f; // NOUVEAU : 0 par défaut
+        this.rotation = 0f; 
         
-        // Déduction automatique du type pour ne pas casser le reste du projet
         if (nom != null) {
             String nomMinuscule = nom.toLowerCase();
             if (nomMinuscule.contains("texte")) {
@@ -38,13 +38,13 @@ public class ObjetBase {
         }
     }
 
-    // Méthode de clonage avec prise en charge de la rotation
     public ObjetBase clonerProfond() {
         ObjetBase copie = new ObjetBase(this.nom, this.x, this.y, this.largeur, this.hauteur);
-        copie.id = this.id; // IMPORTANT : Garder le même ID pour que le moteur retrouve ses références
+        copie.id = this.id; 
         copie.type = this.type;
         copie.contenuTexte = this.contenuTexte;
-        copie.rotation = this.rotation; // NOUVEAU
+        copie.rotation = this.rotation; 
         return copie;
     }
 }
+// bas 1
