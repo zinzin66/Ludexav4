95872153db35130713856c68e0ec6b6796a78052
Zinzin <candy.thoury@gmail.com>
2026-07-29 23:34:53 +0200

    Update visibility logic and enable text keyboard usage

--- app/src/main/java/com/ludexa/moteur/NoeudActionVisibilite.java
+++ app/src/main/java/com/ludexa/moteur/NoeudActionVisibilite.java
@@ -20,7 +20,6 @@ public class NoeudActionVisibilite extends NoeudBase {
         ObjetBase cibleActuelle = getCibleObjet();
         if (cibleActuelle != null && valeurVisible != null) {
             String val = valeurVisible.trim().toLowerCase();
-            // Accepte true, vrai ou oui comme valeurs positives
             boolean estVisible = val.equals("true") || val.equals("vrai") || val.equals("oui");
             cibleActuelle.visible = estVisible;
         }
@@ -28,17 +27,7 @@ public class NoeudActionVisibilite extends NoeudBase {
     }
 
     @Override
-    public boolean utiliseClavierTexte() { return false; } 
-
-    @Override
-    public String getTypeEditeurParametre(String nom) {
-        if ("Visible".equals(nom)) {
-            // Utilisation d'un type booléen si géré par EditeurNoeudDialog
-            // Remplacez TYPE_BOOLEEN par la constante exacte si elle diffère (ex: NoeudBase.TYPE_BOOLEEN)
-            return "TYPE_BOOLEEN"; 
-        }
-        return super.getTypeEditeurParametre(nom);
-    }
+    public boolean utiliseClavierTexte() { return true; }
 
     @Override
     public List<String> getNomsParametres() { 
@@ -56,33 +45,32 @@ public class NoeudActionVisibilite extends NoeudBase {
     }
 
     @Override
-    public boolean requiertCibleObjet() { return true; }[span_1](start_span)[span_1](end_span)
+    public boolean requiertCibleObjet() { return true; }
     
     @Override
     public void setCibleObjet(ObjetBase objet) { 
-        this.cible = objet;[span_2](start_span)[span_2](end_span)
-        this.nomCibleObjet = (objet != null) ? objet.nom : null;[span_3](start_span)[span_3](end_span)
+        this.cible = objet;
+        this.nomCibleObjet = (objet != null) ? objet.nom : null;
     }
     
     @Override
     public ObjetBase getCibleObjet() {
-        if (cible == null && nomCibleObjet != null && contexteApplication != null) {[span_4](start_span)[span_4](end_span)
-            // Reconnexion dynamique[span_5](start_span)[span_5](end_span)
-            try {[span_6](start_span)[span_6](end_span)
-                if (contexteApplication instanceof InterfaceEditeur) {[span_7](start_span)[span_7](end_span)
-                    Scene s = ((InterfaceEditeur) contexteApplication).sceneActive;[span_8](start_span)[span_8](end_span)
-                    if (s != null && s.objets != null) {[span_9](start_span)[span_9](end_span)
-                        for (ObjetBase o : s.objets) if (o.nom.equals(nomCibleObjet)) cible = o;[span_10](start_span)[span_10](end_span)
-                    }[span_11](start_span)[span_11](end_span)
-                } else {[span_12](start_span)[span_12](end_span)
-                    java.lang.reflect.Field sceneField = contexteApplication.getClass().getField("sceneActive");[span_13](start_span)[span_13](end_span)
-                    Scene s = (Scene) sceneField.get(contexteApplication);[span_14](start_span)[span_14](end_span)
-                    if (s != null && s.objets != null) {[span_15](start_span)[span_15](end_span)
-                        for (ObjetBase o : s.objets) if (o.nom.equals(nomCibleObjet)) cible = o;[span_16](start_span)[span_16](end_span)
-                    }[span_17](start_span)[span_17](end_span)
-                }[span_18](start_span)[span_18](end_span)
-            } catch (Exception e) {}[span_19](start_span)[span_19](end_span)
-        }[span_20](start_span)[span_20](end_span)
-        return this.cible;[span_21](start_span)[span_21](end_span)
+        if (cible == null && nomCibleObjet != null && contexteApplication != null) {
+            try {
+                if (contexteApplication instanceof InterfaceEditeur) {
+                    Scene s = ((InterfaceEditeur) contexteApplication).sceneActive;
+                    if (s != null && s.objets != null) {
+                        for (ObjetBase o : s.objets) if (o.nom.equals(nomCibleObjet)) cible = o;
+                    }
+                } else {
+                    java.lang.reflect.Field sceneField = contexteApplication.getClass().getField("sceneActive");
+                    Scene s = (Scene) sceneField.get(contexteApplication);
+                    if (s != null && s.objets != null) {
+                        for (ObjetBase o : s.objets) if (o.nom.equals(nomCibleObjet)) cible = o;
+                    }
+                }
+            } catch (Exception e) {}
+        }
+        return this.cible;
     }
-                       }
+}
