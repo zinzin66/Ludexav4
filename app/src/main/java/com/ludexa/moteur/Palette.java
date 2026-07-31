5ba767a53ec083aa282fbf07c6aad7ddf0094f0e
Zinzin <candy.thoury@gmail.com>
2026-07-31 23:27:20 +0200

    Update Palette.java

--- app/src/main/java/com/ludexa/moteur/Palette.java
+++ app/src/main/java/com/ludexa/moteur/Palette.java
@@ -11,7 +11,7 @@ public class Palette {
     public static int texteSelectionne = Color.parseColor("#95DFF5");
     public static int boutonNormal = Color.parseColor("#045A90");
     public static int boutonSurvol = Color.parseColor("#0975B5");
-    public static int canvasFond = Color.parseColor("#071321");
+    public static int canvasFond = Color.parseColor("#022E4D");
     public static int canvasGrille = Color.parseColor("#152B43");
 }
 // bas 1
