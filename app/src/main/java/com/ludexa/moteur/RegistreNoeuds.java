94c70e6954962c0466566d6aefa5860dbffbb0d5
Zinzin <candy.thoury@gmail.com>
2026-08-12 15:46:04 +0200

    Add new node for 'Jouer Animation' to registry

--- app/src/main/java/com/ludexa/moteur/RegistreNoeuds.java
+++ app/src/main/java/com/ludexa/moteur/RegistreNoeuds.java
@@ -66,9 +66,11 @@ public class RegistreNoeuds {
         REGISTRE.add(new InfoNoeud("Jouer un Son", "Audio", "NoeudActionJouerSon"));
         REGISTRE.add(new InfoNoeud("Gérer Musique", "Audio", "NoeudActionMusique"));
 
-        // NOUVEAUX NOEUDS INTÉGRÉS
         REGISTRE.add(new InfoNoeud("Fondu (Alpha)", "Animations", "NoeudActionFondu"));
         REGISTRE.add(new InfoNoeud("Glisser Vers", "Animations", "NoeudActionGlisserVers"));
+        
+        // NOUVEAU NOEUD INTÉGRÉ ICI
+        REGISTRE.add(new InfoNoeud("Jouer Animation", "Animations", "NoeudActionJouerAnimation"));
     }
 
     public static Map<String, List<InfoNoeud>> getNoeudsParCategorie() {
