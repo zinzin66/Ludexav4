2ef35fbc2756735593ad0b0480ea31b3fb48b523
Zinzin <candy.thoury@gmail.com>
2026-08-13 13:18:36 +0200

    Add button state properties and update clone method
    Added new properties for button states and updated cloning method.

--- app/src/main/java/com/ludexa/moteur/ObjetBase.java
+++ app/src/main/java/com/ludexa/moteur/ObjetBase.java
@@ -27,6 +27,11 @@ public class ObjetBase {
     public int couleur = Color.BLUE;
     public String cheminImage = null;
     
+    // NOUVEAU : Etats pour l'Objet Bouton
+    public String cheminImagePresse = null;
+    public String cheminImageDesactive = null;
+    public boolean estDesactive = false;
+    
     public String type = "carre"; 
     public boolean afficherFondColore = true;
     
@@ -41,7 +46,7 @@ public class ObjetBase {
     public String parentId = null;
     public float alpha = 1.0f;
 
-    // NOUVEAU : SYSTÈME D'ANIMATION MULTIPLE
+    // SYSTÈME D'ANIMATION MULTIPLE
     public HashMap<String, List<String>> animations = new HashMap<>();
     public String animationActive = null;
     public int frameCourante = 0;
@@ -79,6 +84,12 @@ public class ObjetBase {
         copie.estZoneDeClic = this.estZoneDeClic;
         copie.couleur = this.couleur;
         copie.cheminImage = this.cheminImage;
+        
+        // Clonage des états du bouton
+        copie.cheminImagePresse = this.cheminImagePresse;
+        copie.cheminImageDesactive = this.cheminImageDesactive;
+        copie.estDesactive = this.estDesactive;
+        
         copie.type = this.type;
         copie.afficherFondColore = this.afficherFondColore;
         copie.contenuTexte = this.contenuTexte;
@@ -130,7 +141,3 @@ public class ObjetBase {
     }
 }
 // bas 1
-
-
-
-
