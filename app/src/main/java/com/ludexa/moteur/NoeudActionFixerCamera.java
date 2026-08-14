9145af68ccb820b4c112f54380fd4973e46068e7
Zinzin <candy.thoury@gmail.com>
2026-08-14 23:57:05 +0200

    Refactor methods and implement NoeudBase methods
    Refactor methods to single-line format and add mandatory methods from NoeudBase.

--- app/src/main/java/com/ludexa/moteur/NoeudActionFixerCamera.java
+++ app/src/main/java/com/ludexa/moteur/NoeudActionFixerCamera.java
@@ -1,6 +1,9 @@
 // haut 1
 package com.ludexa.moteur;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NoeudActionFixerCamera extends NoeudBase {
 
     private ObjetBase cible;
@@ -22,18 +25,25 @@ public class NoeudActionFixerCamera extends NoeudBase {
     }
 
     @Override
-    public boolean requiertCibleObjet() {
-        return true; 
-    }
+    public boolean requiertCibleObjet() { return true; }
 
     @Override
-    public void setCibleObjet(ObjetBase objet) {
-        this.cible = objet;
-    }
+    public void setCibleObjet(ObjetBase objet) { this.cible = objet; }
 
     @Override
-    public ObjetBase getCibleObjet() {
-        return this.cible;
-    }
+    public ObjetBase getCibleObjet() { return this.cible; }
+
+    // --- Méthodes obligatoires de NoeudBase (même vides) ---
+    @Override
+    public List<String> getNomsParametres() { return new ArrayList<>(); }
+
+    @Override
+    public String getValeurParametre(String nom) { return ""; }
+
+    @Override
+    public void setValeurParametre(String nom, String valeur) {}
+
+    @Override
+    public boolean utiliseClavierTexte() { return false; }
 }
 // bas 1
