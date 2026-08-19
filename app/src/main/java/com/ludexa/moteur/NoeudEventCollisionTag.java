6250ff08b39c4a778779ee4f21a74104940a1ea6
Zinzin <candy.thoury@gmail.com>
2026-08-19 19:24:08 +0200

    Add NoeudEventCollisionTag class for collision handling

---
+++ app/src/main/java/com/ludexa/moteur/NoeudEventCollisionTag.java
@@ -0,0 +1,74 @@
+package com.ludexa.moteur;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoeudEventCollisionTag extends NoeudBase {
+    
+    private transient ObjetBase cible;
+    private String nomCibleObjet;
+    private String tagCible = "";
+    private transient boolean etaitEnCollision = false;
+
+    public NoeudEventCollisionTag() {
+        super(genererId(), "Si objet touche Tag", "Logique & Conditions");
+        this.ajouterPort(new Port("Sortie", Port.TYPE_EXECUTION_SORTIE));
+    }
+
+    @Override
+    public void executer() {
+        propagerExecution("Sortie");
+    }
+
+    @Override
+    public List<String> getNomsParametres() {
+        List<String> params = new ArrayList<>();
+        params.add("Tag");
+        return params;
+    }
+
+    @Override
+    public String getValeurParametre(String nom) {
+        if ("Tag".equals(nom)) return tagCible;
+        return "";
+    }
+
+    @Override
+    public void setValeurParametre(String nom, String valeur) {
+        if ("Tag".equals(nom)) this.tagCible = valeur;
+    }
+
+    @Override
+    public boolean requiertCibleObjet() { return true; }
+
+    @Override
+    public void setCibleObjet(ObjetBase objet) {
+        this.cible = objet;
+        this.nomCibleObjet = (objet != null) ? objet.nom : null;
+    }
+
+    @Override
+    public ObjetBase getCibleObjet() {
+        if (cible == null && nomCibleObjet != null && contexteApplication != null) {
+            try {
+                if (contexteApplication instanceof InterfaceEditeur) {
+                    Scene s = ((InterfaceEditeur) contexteApplication).sceneActive;
+                    if (s != null && s.objets != null) {
+                        for (ObjetBase o : s.objets) if (o.nom.equals(nomCibleObjet)) cible = o;
+                    }
+                } else {
+                    java.lang.reflect.Field sceneField = contexteApplication.getClass().getField("sceneActive");
+                    Scene s = (Scene) sceneField.get(contexteApplication);
+                    if (s != null && s.objets != null) {
+                        for (ObjetBase o : s.objets) if (o.nom.equals(nomCibleObjet)) cible = o;
+                    }
+                }
+            } catch (Exception e) {}
+        }
+        return this.cible;
+    }
+
+    public boolean isEtaitEnCollision() { return etaitEnCollision; }
+    public void setEtaitEnCollision(boolean etat) { this.etaitEnCollision = etat; }
+    public String getTagCible() { return tagCible; }
+}
