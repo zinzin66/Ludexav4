8905d996f29039ae150c412673ac347a7d97b28d
Zinzin <candy.thoury@gmail.com>
2026-08-19 10:29:03 +0200

    Add NoeudActionParallaxe class for parallax actions

---
+++ app/src/main/java/com/ludexa/moteur/NoeudActionParallaxe.java
@@ -0,0 +1,35 @@
+// haut 1
+package com.ludexa.moteur;
+
+public class NoeudActionParallaxe extends NoeudBase {
+    private ObjetBase cibleObj;
+
+    public NoeudActionParallaxe() {
+        super(genererId(), Traducteur.get("noeud_parallaxe"), Traducteur.get("cat_apparence_objets"));
+        ajouterPort(new Port(Traducteur.get("port_entree"), Port.TYPE_EXECUTION_ENTREE));
+        ajouterPort(new Port(Traducteur.get("port_sortie"), Port.TYPE_EXECUTION_SORTIE));
+
+        ajouterParametre("Facteur (ex: 0.5)", "1.0", TYPE_NOMBRE);
+    }
+
+    @Override
+    public void executer() {
+        ObjetBase cible = getCibleObjet();
+        if (cible != null) {
+            try {
+                cible.facteurParallaxe = Float.parseFloat(getValeurParametre("Facteur (ex: 0.5)"));
+            } catch (Exception e) {}
+        }
+        propagerExecution(Traducteur.get("port_sortie"));
+    }
+
+    @Override
+    public boolean requiertCibleObjet() { return true; }
+    
+    @Override
+    public void setCibleObjet(ObjetBase objet) { this.cibleObj = objet; }
+    
+    @Override
+    public ObjetBase getCibleObjet() { return this.cibleObj; }
+}
+// bas 1
