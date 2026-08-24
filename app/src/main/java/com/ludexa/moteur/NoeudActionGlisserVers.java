179b7ac0182fc0cbf52dcd547c26dcce3d7e177b
Zinzin <candy.thoury@gmail.com>
2026-08-24 10:37:08 +0200

    Refactor parameters handling in NoeudActionGlisserVers

--- app/src/main/java/com/ludexa/moteur/NoeudActionGlisserVers.java
+++ app/src/main/java/com/ludexa/moteur/NoeudActionGlisserVers.java
@@ -1,31 +1,28 @@
 // haut 1
 package com.ludexa.moteur;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class NoeudActionGlisserVers extends NoeudBase {
-
-    private transient ObjetBase cible;
-    // SUPPRIMÉ : private String nomCibleObjet;
-    private String cibleX = "0";
-    private String cibleY = "0";
-    private String duree = "1.0";
 
     public NoeudActionGlisserVers() {
         super(genererId(), "Glisser Vers", "Animations");
         this.ajouterPort(new Port("Entrer", Port.TYPE_EXECUTION_ENTREE));
         this.ajouterPort(new Port("Suivant", Port.TYPE_EXECUTION_SORTIE));
+        
+        this.ajouterParametre("Cible X", "0", TYPE_NOMBRE);
+        this.ajouterParametre("Cible Y", "0", TYPE_NOMBRE);
+        this.ajouterParametre("Durée (secondes)", "1.0", TYPE_NOMBRE);
     }
 
     @Override
     public void executer() {
+        // Le ciblage contextuel (tag ou nom) est géré nativement par NoeudBase
         ObjetBase obj = getCibleObjet();
+        
         if (obj != null && contexteApplication != null) {
             try {
-                float targetX = Float.parseFloat(cibleX);
-                float targetY = Float.parseFloat(cibleY);
-                float temps = Float.parseFloat(duree);
+                float targetX = Float.parseFloat(getValeurParametre("Cible X"));
+                float targetY = Float.parseFloat(getValeurParametre("Cible Y"));
+                float temps = Float.parseFloat(getValeurParametre("Durée (secondes)"));
                 
                 android.os.Handler handler = new android.os.Handler(contexteApplication.getMainLooper());
                 handler.post(() -> {
@@ -46,55 +43,8 @@ public class NoeudActionGlisserVers extends NoeudBase {
     }
 
     @Override
-    public List<String> getNomsParametres() { return Arrays.asList("Cible X", "Cible Y", "Durée (secondes)"); }
-    @Override
-    public String getValeurParametre(String nom) {
-        if ("Cible X".equals(nom)) return cibleX;
-        if ("Cible Y".equals(nom)) return cibleY;
-        if ("Durée (secondes)".equals(nom)) return duree;
-        return "";
-    }
-    @Override
-    public void setValeurParametre(String nom, String valeur) {
-        if ("Cible X".equals(nom)) cibleX = valeur;
-        if ("Cible Y".equals(nom)) cibleY = valeur;
-        if ("Durée (secondes)".equals(nom)) duree = valeur;
-    }
-    @Override
     public boolean requiertCibleObjet() { return true; }
     
-    @Override
-    public void setCibleObjet(ObjetBase objet) {
-        this.cible = objet;
-        this.nomCibleObjet = (objet != null) ? objet.nom : null;
-    }
-    
-    @Override
-    public ObjetBase getCibleObjet() {
-        if ("__OBJET_IMPLIQUE__".equals(nomCibleObjet)) return MoteurLogique.dernierObjetImplique;
-
-        if (cible == null && nomCibleObjet != null && contexteApplication != null) {
-            try {
-                if (contexteApplication instanceof InterfaceEditeur) {
-                    InterfaceEditeur editeur = (InterfaceEditeur) contexteApplication;
-                    if (editeur.sceneActive != null && editeur.sceneActive.objets != null) {
-                        for (ObjetBase o : editeur.sceneActive.objets) {
-                            if (nomCibleObjet.equals(o.nom)) { cible = o; break; }
-                        }
-                    }
-                } else {
-                    java.lang.reflect.Field sceneField = contexteApplication.getClass().getField("sceneActive");
-                    Scene s = (Scene) sceneField.get(contexteApplication);
-                    if (s != null && s.objets != null) {
-                        for (ObjetBase o : s.objets) {
-                            if (nomCibleObjet.equals(o.nom)) { cible = o; break; }
-                        }
-                    }
-                }
-            } catch (Exception e) {}
-        }
-        return cible;
-    }
     @Override
     public boolean utiliseClavierTexte() { return true; }
 }
