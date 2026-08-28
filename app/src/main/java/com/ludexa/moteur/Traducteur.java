4f9aba8d2c1e7cada83f2b6d2ab978ed4a973b32
Yop2d-dev <candy.thoury@gmail.com>
2026-08-29 00:55:57 +0200

    Add new translations for scene editing errors

--- app/src/main/java/com/ludexa/moteur/Traducteur.java
+++ app/src/main/java/com/ludexa/moteur/Traducteur.java
@@ -104,6 +104,9 @@ public class Traducteur {
         if (cle.equals("obj_prefix_prefab")) return "Prefab";
         if (cle.equals("titre_select_scene_liee")) return "Sélectionner une scène à lier";
         if (cle.equals("erreur_aucune_autre_scene")) return "Aucune autre scène disponible.";
+        if (cle.equals("insp_btn_editer_scene_liee")) return "Éditer la scène source ➔";
+        if (cle.equals("insp_erreur_scene_introuvable")) return "Erreur : Scène introuvable";
+        
 
         return "[" + cle + "]";
     }
