225695c7220e5c6ecd4b4c2abf074cf8135c933e
Yop2d-dev <candy.thoury@gmail.com>
2026-08-28 16:53:45 +0200

    Add translations for prefabs and scene selection

--- app/src/main/java/com/ludexa/moteur/Traducteur.java
+++ app/src/main/java/com/ludexa/moteur/Traducteur.java
@@ -100,6 +100,11 @@ public class Traducteur {
         if (cle.equals("cat_animations")) return "Animations";
         if (cle.equals("cat_physique")) return "Physique";
 
+        // --- PREFABS & SCENES IMBRIQUEES ---
+        if (cle.equals("obj_prefix_prefab")) return "Prefab";
+        if (cle.equals("titre_select_scene_liee")) return "Sélectionner une scène à lier";
+        if (cle.equals("erreur_aucune_autre_scene")) return "Aucune autre scène disponible.";
+
         return "[" + cle + "]";
     }
 
